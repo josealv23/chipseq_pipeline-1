0ed32859c32fcb5b8fc9e32d2ccb300308f5130b
lauracs7 <44810554+lauracs7@users.noreply.github.com>
2018-11-27 19:58:20 +0100

    Update read_mapping.sh

--- read_mapping.sh
+++ read_mapping.sh
@@ -1,41 +1,62 @@
-#$-S /bin/bash 
-#$ -cwd 
+#$ -S /bin/bash
+#$ -cwd
 #$ -j yes
 #$ -V
 
 #Parámetros de entrada
+
 SAMPLE_DIR=$1
 SAMPLE_FILE=$2
 SAMPLE_TYPE=$3
 SAMPLES_DONE=$4
+OUTPUT=$5
+INS=$6
+MAIN_FOLDER=$7
+WD=$8
+
 
 #
 cd ${SAMPLE_DIR}
 
 cp ${SAMPLE_FILE} ${SAMPLE_TYPE}.fq
 
-#Procesamiento de la muestra 
+#Procesamiento de la muestra
 bowtie -S -v 2 --best --strata -m1 ../../genome/index ${SAMPLE_TYPE}.fq > ${SAMPLE_TYPE}.sam
 
 #Escribimos en la pizarra
 
 echo ${SAMPLE_TYPE} DONE >> ../../log/blackboard
 
-SAMPLES_DONE=$(wc -l ../../log/blackboard | awk '{print $1}’)
-cp ${SAMPLE_FILE} ${SAMPLE_TYPE}.fq
+SAMPLES_DONE=$(wc -l ../../log/blackboard | awk '{ print $1 }' )
 
-#Procesamiento de la muestra 
-bowtie -S -v 2 --best --strata -m1 ../../genome/index ${SAMPLE_TYPE}.fq > ${SAMPLE_TYPE}.sam
-
-#Escribimos en la pizarra
-
-echo ${SAMPLE_TYPE} DONE >> ../../log/blackboard
-
-SAMPLES_DONE=$(wc -l ../../log/blackboard | awk '{print $1}')
-
-if [$SAMPLES_DONE -eq 2]
-then 
-        qsub -N peak_calling -o peak_calling  $INS/peak_calling.sh $WD/$MAIN_FOLDER/results 
+if [ $SAMPLES_DONE -eq 2 ]
+then
+echo "entra en if del readmapping" >> ../../log/debug
+        qsub -N peak_calling -o peak_calling ${INS}/peak_calling.sh ${INS} ${WD}/${MAIN_FOLDER}/results ${WD}/${MAIN_FOLDER}/annotation/annotation.gtf ${OUTPUT} $WD $MAIN_FOLDER
+echo 'Todo ok con read_mapping' >> ../../log/debug
+echo '$INS:' >> ../../log/debug
+echo ${INS} >> ../../log/debug
+echo '$WD:' >> ../../log/debug
+echo ${WD} >> ../../log/debug
+echo '$MAIN_FOLDER:' >> ../../log/debug
+echo ${MAIN_FOLDER} >> ../../log/debug
+echo '$OUTPUT:' >> ../../log/debug
+echo ${OUTPUT} >> ../../log/debug
 fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
