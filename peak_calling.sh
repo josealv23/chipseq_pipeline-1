ec4e3e787454fd73b5105c86a632be84c5c7da55
josealv23 <44974717+josealv23@users.noreply.github.com>
2018-12-07 18:35:08 +0100

    Minor bug fixes, commented script

--- peak_calling.sh
+++ peak_calling.sh
@@ -3,44 +3,30 @@
 #$ -j yes
 #$ -V
 
-echo 'Todo OK antes de leer parámetros' >> /home/bag1/temp/prr5_samples/log/debug
-
-#Parámetros de entrada
+############################
+### Parameters and input ###
+############################
 
 RES=$1
-echo '$RES:' >> /home/bag1/temp/prr5_samples/log/debug
-echo $RES >> /home/bag1/temp/prr5_samples/log/debug
 ANNO=$2
-echo '$ANNO:' >> /home/bag1/temp/prr5_samples/log/debug
-echo $ANNO >> /home/bag1/temp/prr5_samples/log/debug
-OUTPUT=$3
-echo '$OUTPUT:' >> /home/bag1/temp/prr5_samples/log/debug
-echo $OUTPUT >> /home/bag1/temp/prr5_samples/log/debug
-WD=$4
-echo '$WD:' >> /home/bag1/temp/prr5_samples/log/debug
-echo $WD >> /home/bag1/temp/prr5_samples/log/debug
-MAIN_FOLDER=$5
-echo '$MAIN_FOLDER:' >> /home/bag1/temp/prr5_samples/log/debug
-echo $MAIN_FOLDER >> /home/bag1/temp/prr5_samples/log/debug
-INS=$6
-echo '$INS:' >> /home/bag1/temp/prr5_samples/log/debug
-echo $INS >> /home/bag1/temp/prr5_samples/log/debug
-NDG=$7
-OVERLAP=$8
-echo $NDG >> /home/bag1/temp/prr5_samples/log/debug
-echo $OVERLAP >> /home/bag1/temp/prr5_samples/log/debug
+WD=$3
+MAIN_FOLDER=$4
+INS=$5
 
-#MACS2
+
+####################
+### Peak calling ###
+####################
+
+# MACS2
 
 cd $MAIN_FOLDER/results
+macs2 callpeak -t ../samples/chip/chip.sam -c ../samples/input/input.sam -f SAM --outdir . -n peak
 
-macs2 callpeak -t ../samples/chip/chip.sam -c ../samples/input/input.sam -f SAM --outdir . -n prr5
+# PeakAnnotator
 
+java -jar $INS/PeakAnnotator.jar -u NDG -g all -p peak_summits.bed -a $ANNO -o .
 
-#PeakAnnotator
-java -jar $INS/PeakAnnotator.jar -u NDG -g all -p prr5_summits.bed -a $ANNO -o .
+# Run R scriptn R
 
-
-#Ejecutar script en R
-
-Rscript --vanilla $INS/target_genes.R prr5_summits.overlap.bed prr5_summits.ndg.bed $OUTPUT
+Rscript --vanilla $INS/target_genes.R peak_summits.overlap.bed peak_summits.ndg.bed peak_target_genes.txt
