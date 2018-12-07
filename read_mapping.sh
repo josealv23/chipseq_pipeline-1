8f96ac2eae0b7e9ee97c445dd1e0d426e9d8d254
josealv23 <44974717+josealv23@users.noreply.github.com>
2018-12-07 18:36:44 +0100

    Bug fixes, commented script

--- read_mapping.sh
+++ read_mapping.sh
@@ -3,54 +3,43 @@
 #$ -j yes
 #$ -V
 
-#Parámetros de entrada
+#############################
+### Parameters and inputs ###
+#############################
 
 SAMPLE_DIR=$1
 SAMPLE_FILE=$2
 SAMPLE_TYPE=$3
-OUTPUT=$4
-MAIN_FOLDER=$5
-WD=$6
-INS=$7
-NDG=$8
-OVERLAP=$9
+MAIN_FOLDER=$4
+WD=$5
+INS=$6
 
-echo $WD >> ../../log/debug
 
-#
+#############################
+### Workspace preparation ###
+#############################
+
 cd ${SAMPLE_DIR}
-
 cp ${SAMPLE_FILE} ${SAMPLE_TYPE}.fq
 
-#Procesamiento de la muestra
+
+#################################################################################
+### Sample processing and checking if the rest of samples have been processed ###
+#################################################################################
+
+# Sample processing
+
 bowtie -S -v 2 --best --strata -m1 ../../genome/index ${SAMPLE_TYPE}.fq > ${SAMPLE_TYPE}.sam
 
-#Escribimos en la pizarra
+# Write in the blackboard
 
 echo ${SAMPLE_TYPE} DONE >> ../../log/blackboard
+
+# Check if all samples have been processed - if they have been processed, run peak calling script
 
 SAMPLES_DONE=$(wc -l ../../log/blackboard | awk '{ print $1 }' )
 
 if [ $SAMPLES_DONE -eq 2 ]
 then
-qsub -N peak_calling -o peak_calling ${INS}/peak_calling.sh ${MAIN_FOLDER}/results ${MAIN_FOLDER}/annotation/annotation.gtf ${OUTPUT} $WD $MAIN_FOLDER $INS $NDG $OVERLAP
-echo 'into the if of peak_calling' >> ../../log/debug
-echo '$INS:' >> ../../log/debug
-echo ${INS} >> ../../log/debug
-echo '$WD:' >> ../../log/debug
-echo ${WD} >> ../../log/debug
-echo '$MAIN_FOLDER:' >> ../../log/debug
-echo ${MAIN_FOLDER} >> ../../log/debug
-echo '$OUTPUT:' >> ../../log/debug
-echo ${OUTPUT} >> ../../log/debug
+qsub -N peak_calling -o peak_calling ${INS}/peak_calling.sh ${MAIN_FOLDER}/results ${MAIN_FOLDER}/annotation/annotation.gtf $WD $MAIN_FOLDER $INS
 fi
-
-
-
-
-
-
-
-
-
-
